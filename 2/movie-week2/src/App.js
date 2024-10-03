724cd9c6448b0a22f515436885ef168932675dac
숨 <stacycho0728@naver.com>
2024-10-04 03:10:43 +0900

    [week2/mission] Todo List(CSS), 영화 컴포넌트

--- 2/movie-week2/src/App.js
+++ 2/movie-week2/src/App.js
@@ -6,14 +6,7 @@ function App() {
     <div className="App">
       <div className="app-container">
         {movies.results.map((item) => {
-          return (
-            <Movie
-              title={item.title}
-              poster_path={item.poster_path}
-              vote_average={item.vote_average}
-              overview={item.overview}
-            />
-          );
+          return <Movie key={item.id} poster_path={item.poster_path} />;
         })}
       </div>
     </div>
--- 2/movie-week2/src/Components/Movie.jsx
+++ 2/movie-week2/src/Components/Movie.jsx
@@ -1,33 +1,9 @@
-import React, { useState } from "react";
-import Description from "./Description";
+import React from "react";
 
-const ING_BASE_URL = "https://image.tmdb.org/t/p/w1280/";
-function Movie({ title, poster_path, vote_average, overview }) {
-  const [isMouseOver, setIsMouseOver] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsMouseOver(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsMouseOver(false);
-  };
-
+export default function Movie({ poster_path }) {
   return (
-    <div
-      class="movie-container"
-      onMouseEnter={handleMouseEnter}
-      onMouseOut={handleMouseLeave}
-    >
-      <img src={poster_path} alt="영화포스터" />
-
-      {isMouseOver && <Description title={title} overview={overview} />}
-      <div class="movie-info">
-        <h4>{title}</h4>
-        <span> {vote_average}</span>
-      </div>
+    <div className="movie-container">
+      <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} />
     </div>
   );
 }
-
-export default Movie;
