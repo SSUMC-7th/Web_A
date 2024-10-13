bc951ee396c6986a768962c9594c7a186655dd71
DeoKanKoong <rla9046@gmail.com>
2024-10-13 15:52:33 +0900

    File 수정

--- 05/src/components/custom-button.jsx
+++
@@ -1,9 +0,0 @@
-const CustomButton = () => {
-    return (
-        <button>
-            커스텀 버튼
-        </button>
-    );
-};
-
-export default CustomButton;
--- 05/src/pages/not-found.jsx
+++
@@ -1,7 +0,0 @@
-const NotFound = () => {
-    return (
-        <h1>제대로 좀 해라</h1>
-    );
-};
-
-export default NotFound;
