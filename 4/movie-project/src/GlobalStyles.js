418f1bebe4d52db98064d395c57e8077e6f830e5
duwlsssss <rladuwls0814@gmail.com>
2024-10-20 21:21:38 +0900

    [week4/mission] 반응형 디자인 수정

--- 4/movie-project/src/GlobalStyles.js
+++ 4/movie-project/src/GlobalStyles.js
@@ -6,10 +6,31 @@ const GlobalStyles = createGlobalStyle`
   *{
     box-sizing: border-box;
   }
+  @font-face {
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 300;
+  src: url("/fonts/Pretendard-Light.woff2") format("woff2"), 
+       url("/fonts/Pretendard-Light.woff") format("woff");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 500;
+    src: url("/fonts/Pretendard-Regular.woff2") format("woff2"), 
+        url("/fonts/Pretendard-Regular.woff") format("woff");
+  }
+  @font-face {
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 700;
+    src: url("/fonts/Pretendard-Bold.woff2") format("woff2"), 
+        url("/fonts/Pretendard-Bold.woff") format("woff");
+  }
   body {
     color: white;
     background-color: black;
-    font-family: Arial, sans-serif;
+    font-family: "Pretendard", sans-serif;
   }
   img{
     display:block;
