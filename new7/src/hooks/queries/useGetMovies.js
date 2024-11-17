51fd2c85c751345ce82a0261f9c8303ed82ccc34
숨 <stacycho0728@naver.com>
2024-11-17 20:07:03 +0900

    pagination 구현

--- new7/src/hooks/queries/useGetMovies.js
+++ new7/src/hooks/queries/useGetMovies.js
@@ -1,11 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "../../apis/axios-instance";
 
-const useGetMovies = async ({ category, pageParam }) => {
-  const { data } = await axiosInstance.get(
-    `/movie/${category}?language=ko-KRS=${pageParam}`
-  );
-
-  return data;
+const useGetMovies = (category, page) => {
+  return useQuery({
+    queryKey: ["movies", category, page],
+    queryFn: async () => {
+      try {
+        const { data } = await axiosInstance.get(`/movie/${category}`, {
+          params: { page }, // 페이지 번호 전달
+        });
+        return data;
+      } catch (error) {
+        console.error(
+          "Error fetching movies:",
+          error.response || error.message
+        );
+        throw new Error("Failed to fetch movies");
+      }
+    },
+    keepPreviousData: true, // 이전 페이지 데이터를 유지
+  });
 };
 
 export { useGetMovies };
